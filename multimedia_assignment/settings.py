b4fb4b4a8ee02411eaf5eeaff758edf588bb1f35
Ridge Tejuco <39019291+RidgeGT@users.noreply.github.com>
2021-08-15 01:08:19 -0700

    Update settings.py

--- multimedia_assignment/settings.py
+++ multimedia_assignment/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure--6ivz0^!n#n45v%5dg0)6y+1kqs8ez9#tv$80+&&z+)l7l7@jg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
